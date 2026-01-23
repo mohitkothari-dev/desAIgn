3f589b3c94f4862aa95f6609916da6a566104570
mohitkothari-dev <mrulekothari@gmail.com>
2026-01-23 19:49:59 +0530

    added sidebar and project page

---
+++ app/dashboard/project/[projectId]/page.tsx
@@ -0,0 +1,12 @@
+
+
+const ProjectCanvasPlayground = () => {
+  return (
+    <div>
+      {/* ShadCN UI sidebar */}
+      {/* ShadCN UI sheet: use symbols at the bottom of the screen to open the sheet  */}
+    </div>
+  )
+}
+
+export default ProjectCanvasPlayground
